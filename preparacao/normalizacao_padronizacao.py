bba40a1094228174ed098c6010d790bd3850ef06
jaoaff <101889176+jaoaff@users.noreply.github.com>
2026-02-24 19:48:41 -0300

    modificações

--- preparacao/normalizacao_padronizacao.py
+++ preparacao/normalizacao_padronizacao.py
@@ -30,3 +30,12 @@ df['idadeRobustScaler'] = scaler.fit_transform(df[['idade']])
 df['salarioRobustScaler'] = scaler.fit_transform(df[['salario']])
 
 print(df.head(15))
+
+print("MinMaxScaler (De 0 a 1):")
+print("Idade - Min: {:.4f} Max: {:.4f} Mean: {:.4f} Str: {:.4f}".format(df['idadeMinMaxScaler'].min(), df['idadeMinMaxScaler'].max(), df['idadeMinMaxScaler'].mean(), df['idadeMinMaxScaler'].std()) )
+print("Salário - Min: {:.4f} Max: {:.4f} Mean: {:.4f} Str: {:.4f}".format(df['salarioMinMaxScaler'].min(), df['salarioMinMaxScaler'].max(), df['salarioMinMaxScaler'].mean(), df['salarioMinMaxScaler'].std()))
+
+print("\nMinMaxScaler (De -1 a 1):")
+print("Idade - Min: {:.4f} Max: {:.4f} Mean: {:.4f} Str: {:.4f}".format(df['idadeMinMaxScaler_mm'].min(), df['idadeMinMaxScaler_mm'].max(), df['idadeMinMaxScaler_mm'].mean(), df['idadeMinMaxScaler_mm'].std()) )
+print("Salário - Min: {:.4f} Max: {:.4f} Mean: {:.4f} Str: {:.4f}".format(df['salarioMinMaxScaler_mm'].min(), df['salarioMinMaxScaler_mm'].max(), df['salarioMinMaxScaler_mm'].mean(), df['salarioMinMaxScaler_mm'].std()))
+
